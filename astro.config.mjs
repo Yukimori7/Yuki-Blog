ca2d75eae9e320df072ed2ba6f373a9466e25926
Yukimori7 <i@tawawa.moe>
2025-11-22 17:39:39 +0800

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://example.com',
+
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
