4cd668017ad28a84577d0b09522bcf2a8087d524
Yukimori7 <i@tawawa.moe>
2026-04-23 02:01:45 +0800

    update: "project change"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,5 @@
 // @ts-check
-
-import cloudflare from '@astrojs/cloudflare'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'static',
-
-  site: 'https://tawawa.moe',
-
-  // 使用 Cloudflare 适配器
-  adapter: cloudflare({
-    imageService: 'compile', // 静态站点在构建时处理图片
-  }),
-
-  integrations: [icon()],
-
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-    },
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-})
+export default defineConfig({});
