9c8e7d61ee0baa3b74761ae9bf25c61fc723d95a
Yukimori7 <i@tawawa.moe>
2026-04-23 14:30:50 +0800

    feat: add sync github issues script

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection } from 'astro:content'
+import { glob } from 'astro/loaders'
+import { z } from 'astro/zod'
+
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    issueId: z.number(),
+    issueNumber: z.number(),
+    state: z.string(),
+    createAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+    auther: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+  }),
+})
+
+export const collections = { blog }
